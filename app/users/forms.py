56b7d0a7e69804e2f02edbabb482d5913eb8ad4f
Peter Uhlenbruck <peuhlenb@gmail.com>
2016-05-17 22:59:33 -0400

    static css loads correctly on all pages

--- app/users/forms.py
+++ app/users/forms.py
@@ -3,12 +3,11 @@ from wtforms.fields import TextField, PasswordField
 from wtforms.validators import Required
 
 class LoginForm(Form):
-	username = TextField('username', validators = [Required()])
-	password = PasswordField('password', validators = [Required()])
+	username = TextField('username', validators = [Required()], description='username')
+	password = PasswordField('password', validators = [Required()], description='password')
 
 
 class RegisterForm(Form):
-    username = TextField('username', validators = [Required()])
-    password = PasswordField('password', validators = [Required()])
-    password_confirm = PasswordField('password', validators = [Required()])
-	
+    username = TextField('username', validators = [Required()], description='username')
+    password = PasswordField('password', validators = [Required()], description='password')
+    password_confirm = PasswordField('password', validators = [Required()], description='repeat password')
