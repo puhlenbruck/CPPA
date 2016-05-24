b1500ff0fb357689ba8fdaae6c00690cdd884f3b
Peter Uhlenbruck <peuhlenb@gmail.com>
2016-05-23 20:05:17 -0400

    create default skills table

--- app/characters/character/skills.py
+++ app/characters/character/skills.py
@@ -1,1 +1,11 @@
- 
+Skills=[
+    {'name':'Attractivnes','abbr':'ATTR', 'skills':['Personal Grooming', 'Wardrobe & Style']},
+    {'name':'Body','abbr':'BODY', 'skills':['Endurance', 'Strength Feat', 'Swimming']},
+    {'name':'Cool','abbr':'COOL', 'skills':['Interrogation', 'Intimidate', 'Oratory', 'Resist Torture/Drugs', 'Streetwise']},
+    {'name':'Empathy','abbr':'EMP', 'skills':['Human Perception','Interview','Leadership','Seduction','Social','Persuasion & Fast Talk','Perform']},
+    {'name':'Intelligence','abbr':'INT', 'skills':['Accounting','Anthropology','Awareness/Notice','Biology','Botany','Chemistry','Composition','Diagnose Illness','Education & General Knowledge','Expert','Gamble','Geology','Hide/Evade','History','Language','Library Search','Mathematics','Physics','Programming','Stock Market','System Knowledge','Teaching','Wilderness Survival','Zoology']},
+    {'name':'Luck','abbr':'LUCK', 'skills':[]},
+    {'name':'Movement Allowance','abbr':'MA', 'skills':[]},
+    {'name':'Reflex','abbr':'REF', 'skills':['Archery','Athletics','Brawling','Dance','Dodge & Escape','Driving','Fencing','Handgun','Heavy Weapons','Martial Art','Melee','Motorcycle','Operate Heavy Machinery','Pilot (Gyro)','Pilot (Fixed Wing)','Pilot (Dirigible)','Pilot (Vector Thrust Vehicle)','Rifle','Stealth','Submachinegun']},
+    {'name':'Tech','abbr':'TECH', 'skills':['Aero Tech','AV Tech','Basic Tech','Cryotank Operation','Cyberdeck Design','Cyber Tech','Demolitions','Disguise','Electronics','Electronic Security','First Aid','Forgery','Gyro Tech','Paint/Draw','Photo & Film','Pharmacuticals','Pick Lock','Pick Pocket','Play Instrument','Weaponsmith']}
+]
--- dbcontrols.py
+++ dbcontrols.py
@@ -1,9 +1,10 @@
 import rethinkdb as r
 from rethinkdb.errors import RqlRuntimeError
 from config import RDB_HOST, RDB_PORT, CP2020_DB
+from app.characters.character.attributes import ATTRIBUTES
 
 
-TABLE_NAMES = ['test', 'users', 'characters']
+TABLE_NAMES = ['users', 'characters','default_skills']
 
 # db setup; only run once
 def db_setup():
@@ -13,6 +14,7 @@ def db_setup():
     connection.use(CP2020_DB)
     table_create(connection)
     table_options(connection)
+    poulate_default_skills(connection)
     connection.close()
     print ('Database setup completed')
 
@@ -23,7 +25,7 @@ def table_create(connection):
             print ('Table ' + table + ' created')
         except RqlRuntimeError:
             print ('Table ' + table + ' already exists')
-            
+
 def table_options(connection):
     try:
         r.table('users').index_create('username').run(connection)
@@ -40,7 +42,17 @@ def table_options(connection):
         print ('character user index added')
     except RqlRuntimeError:
         print ('character user index already exists')
-    
+    try:
+        r.table('default_skills').index_create('attribute').run(connection)
+        print ('default_skill attribute index added')
+    except RqlRuntimeError:
+        print ('default_skill attribute index already exists')
+    try:
+        r.table('default_skills').index_create('name').run(connection)
+        print ('default_skill name index added')
+    except RqlRuntimeError:
+        print ('default_skill name index already exists')
+
 
 def db_create(connection):
     try:
@@ -48,7 +60,7 @@ def db_create(connection):
         print ('Database created')
     except RqlRuntimeError:
         print ('Database already exists')
-        
+
 
 def autoinc_id(table, connection):
      id_selection = table.get_field('id').run(connection)
@@ -56,9 +68,14 @@ def autoinc_id(table, connection):
      new_id = len(id_list)+1
      return new_id
 
+def populate_default_skills(connection):
+    for attr in ATTRIBUTES:
+        for skill in attr['skill']:
+            skill_mapping={'name':skill,'attribute':attr['name']}
+            r.table('default_skills').insert(skill_mapping)
 
 # DANGEROUS
-# drop everything and create a new, empty database      
+# drop everything and create a new, empty database
 def db_reset():
     if yn_choice("Are you sure you wish to reset the Database?\nAll data will be lost and this cannot be undone.", 'n'):
         connection = r.connect(host=RDB_HOST, port=RDB_PORT, db=CP2020_DB)
@@ -70,7 +87,7 @@ def db_reset():
         finally:
             connection.close()
         dbSetup()
-    
+
 def yn_choice(message, default='n'):
     choices = 'Y/n' if default.lower() in ('y', 'yes') else 'y/N'
     choice = input("%s (%s) " % (message, choices))
